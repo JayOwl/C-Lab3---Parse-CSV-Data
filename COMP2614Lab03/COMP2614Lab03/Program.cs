811440a47d0d821f1fccb4178c8acc16b5d70ce0
Joel Benoit <joel.benoit@tsunamisolutions.com>
2019-05-21 17:10:10 -0700

    add the data file.

--- COMP2614Lab03/COMP2614Lab03/Program.cs
+++ COMP2614Lab03/COMP2614Lab03/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace COMP2614Lab03
     {
         static void Main(string[] args)
         {
+            Console.Title = "COMP2614 Lab03";
+
+            StreamReader data = new StreamReader("...\\...\\data.txt");
+
+
+
         }
     }
 }
