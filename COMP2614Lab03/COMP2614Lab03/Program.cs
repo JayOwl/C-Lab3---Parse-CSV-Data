c6e891b5142008f6bf6bdecd4d70072c8f7d23a5
Joel Benoit <joel.benoit@tsunamisolutions.com>
2019-05-21 20:45:55 -0700

    Add stream reader, iterate streamreader with while loop, do perform calculations in while loop.

--- COMP2614Lab03/COMP2614Lab03/Program.cs
+++ COMP2614Lab03/COMP2614Lab03/Program.cs
@@ -13,10 +13,60 @@ namespace COMP2614Lab03
         {
             Console.Title = "COMP2614 Lab03";
 
-            StreamReader data = new StreamReader("...\\...\\data.txt");
+            StreamReader streamReadObjectInstance = new StreamReader("...\\...\\data.txt");
 
+            string relativeFilePath = @"..\..\data.txt";
+            string stringContainer;
 
+            if (!File.Exists(relativeFilePath))
+            {
+                Console.WriteLine("The file was not found, please update the streamreader pointer");
+            }
+            else
+            {
+                Console.WriteLine("{0,-15} {1,-15} {2, 5}", "First Name", "Last Name", "Age");
+                Console.WriteLine(new string('-', 37));
 
+                try
+                {
+                    using (streamReadObjectInstance = new StreamReader(relativeFilePath))
+                    {
+                        int sum = 0;
+                        int count = 0;
+                        double average = 0.0;
+                        while ((stringContainer = streamReadObjectInstance.ReadLine()) != null)
+                        {
+                            string[] memberData = stringContainer.Split(',');
+                            Console.WriteLine("{0,-15} {1,-15} {2,5}",
+                            memberData[0], memberData[1], memberData[2]);                        
+
+                            var summation = Convert.ToInt32(memberData[2]);          
+                            sum += summation;
+
+                            count++;
+
+                            average = (double)sum / count;
+                        }
+                        Console.WriteLine(new string('-', 37));
+                        Console.WriteLine("{0,-15} {1, -15} {2,5}", "", "Total: ", sum);
+                        Console.WriteLine("{0,-15} {1, -15} {2,5}", "", "Count: ", count);
+                        Console.WriteLine("{0,-15} {1, -15} {2,5}", "", "Average: ", average);
+                    }
+                }
+
+                catch (Exception exceptionContainer)
+                {
+                    Console.WriteLine($"\n{exceptionContainer.Message}");
+                }
+
+                finally
+                {
+                    if (streamReadObjectInstance != null)
+                    {
+                        streamReadObjectInstance.Close();
+                    }
+                }                
+            }
         }
     }
 }
